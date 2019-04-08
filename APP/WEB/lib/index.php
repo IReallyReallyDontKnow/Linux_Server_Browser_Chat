cf4ae5dd67608bc72210bf309a5aac07f9612920
SICK IRIS <34299302+ArrayMy@users.noreply.github.com>
2019-04-08 09:50:24 +0200

    Update index.php

--- APP/WEB/lib/index.php
+++ APP/WEB/lib/index.php
@@ -2,6 +2,25 @@
 
 class index
 {
+  public function __construct(){};
+  
+  private function read_config()
+  {
+    /*PARSE INI FILE */
+  
+  }
+  
+  
+  private function check_first_start()
+  {
+    if($this->config['setup'] === true)
+    {
+      $this->started();
+    }else
+    {
+      $this->first_start();
+    }
+  }
 
 }
 
