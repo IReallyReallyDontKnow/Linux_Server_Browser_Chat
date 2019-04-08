56d7059669f97acec4f4f9beefa5d46bc85b9352
SICK IRIS <34299302+ArrayMy@users.noreply.github.com>
2019-04-08 10:27:44 +0200

    Update template.php

--- APP/WEB/lib/template.php
+++ APP/WEB/lib/template.php
@@ -1,7 +1,17 @@
 <?php
-
 class template
 {
+  private $where;
+  private $how;
+  public function __construct($where,$how)
+  {
+    $this->where = $where;
+    $this->how = $how;
+  }
+  private function redirect()
+  {
+    header("LOCATION: _DIR_/$this->where.php?sc=$this->how");
+  }
 }
 
 ?>
