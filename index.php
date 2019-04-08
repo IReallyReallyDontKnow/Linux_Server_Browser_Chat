457227853d18e94b659959f80a92648945f761f5
SICK IRIS <34299302+ArrayMy@users.noreply.github.com>
2019-04-08 10:22:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<?php require_once('APP/WEB/lib/index.php'); $index = new index(); ?>
+<html>
+  <head>
+  <title>BROWSER CHAT | LINUX SERVER</title>
+  <meta charset=utf8>
+  </head>
+  
+  <body>
+    <?php $index->check_first_start(); ?>
+  </body>
+  
+</html>
